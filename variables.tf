91c6edea35cedc1b4e5105c79b1b1be4639ffdbe
ajaysinha1804 <125199089+ajaysinha1804@users.noreply.github.com>
2024-12-13 15:36:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "region" {
   description = "The AWS region to deploy resources in"
   default     = "us-west-2"
 }
-
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   default     = "10.0.0.0/16"
